169c6f4abc505aba24e120aea14484e1a8af1805
Ranko Andric <andere395@gmail.com>
2017-10-30 16:13:25 +0100

    Cas 24 - cubesphdatabase

--- sql/03-SQL-Insert.sql
+++ sql/03-SQL-Insert.sql
@@ -1,3 +1,17 @@
 -- Zadatak: Napisati upit za unos jednog proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, created_at)
+VALUES
+('Apple','iPhone6 S','iPhone6 S','',39999.99,'12.00','Mobilni Telefon',0,0,'2016-03-21 18:00:00');
+
+
+
 
 -- Zadatak: Napisati upit za unpos vise proizvoda
+INSERT INTO products
+(brand, title, description, specification, price, quantity, category, on_sale, discount, created_at)
+VALUES
+('Apple','iPhone6 SE','iPhone6 SE','',49999.99,'12.00','Mobilni Telefon',0,0,'2016-03-21 18:00:00'),
+('Apple','iPhone3A','iPhone3A','',31239.99,'1.00','Mobilni Telefon',0,0,'2016-10-30 11:00:00');
+ 
+ 
--- sql/04-SQL-Update.sql
+++ sql/04-SQL-Update.sql
@@ -1,5 +1,36 @@
 -- Zadatak: Napisati upit koji menja naziv proizvoda u 'HTC U12' za proizvod sa ID-jem 20.
+UPDATE
+    products
+SET
+    title = 'HTC U12'
+WHERE
+    id = 20;              
+
+--Zadatak: Napisati upit koji naziv i opis proizvoda menja u HTC U12 za proizvod sa ID 20
+UPDATE
+    products
+SET
+    title = 'HTC U12',
+    description = 'Mobilni telefon HTC U12'
+WHERE
+    id = 20;
 
 -- Zadatak: Napisati upit koji podize cenu za 200.00 za sve proizvode koji su na rasprodaju
 
+UPDATE
+    products
+SET
+    price = price + 200
+WHERE
+    on_sale = 1;
+
 -- Zadatak: Napisati upit koji sizava cenu za 10% za sve proizvode koji su na rasprodaji i koji su u kategoriji 'Mobilni Telefon'
+
+UPDATE
+    products
+SET
+    price = price * 0.9
+WHERE
+    on_sale = 1 AND category = 'Mobilni Telefon';
+
+
